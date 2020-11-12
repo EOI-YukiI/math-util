5dce0977f63bd398a59b72b6b5e43b16149e30e9
EOI-YukiI <inosiyuki1@gmail.com>
2020-11-12 08:36:32 +0700

    1s upload

---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.util;
+
+import yukii.util.MathUtility;
+
+/**
+ *
+ * @author inosi
+ */
+public class MathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        
+        System.out.println(MathUtility.getFactorial(5));
+        System.out.println(MathUtility.getFactorial(1));
+        System.out.println(MathUtility.getFactorial(15));
+        System.out.println(MathUtility.getFactorial(-1));
+        System.out.println(MathUtility.getFactorial(30));
+        System.out.println(MathUtility.getFactorial(15));
+    }
+
+}
+
+//Code viết ra Dev phải test trước
+//có vài hình thức test code khác nhau
+//1. TDD, Test Driven Development
+//viets dàn khung của hàm getFactorial(int n)
+//sau đó dựng hàm test()
+//chạy thử để coi xanh đỏ, dỏ là lỗi, xanh là ổn
+//cứ liên tục như thế trong quá trình viết code
+//và sửa code, người ta gọi là lập trình hướng theo thỏa mãn/
+//việc test, TDD
+
+//2. sout(hàm kèm dât) để có kết quả ra sao, có đúng như mình kỳ vọng không
+//nó phải trả về như thế này ahay không -> xem thế quả test = mắt
+
+//3. JOtionPane của bên JavaDesktop để popup kết quả lên màn hình 
+//vẫn xem = mắt
+
+//4. viết 1 trnag web gọi hàm xử lí,... hao sức quá
+
+//ta chơi cách một
---
+++ src/yukii/util/MathUtility.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yukii.util;
+
+/**
+ *
+ * @author inosi
+ */
+public class MathUtility {
+    //class này chưa các hàm tiện ích dùng chung cho mọi nơi
+    //do đó nó phải nên là static
+    public static final double PI = 3.1415;
+    
+    //hàm tính n giai thừa. n>= 0 && c<=20
+    //nếu quá 20 thì tràn kiểu long
+    //0! = 1; 1! =1; n! = 1.2.3...n
+    public static long getFactorial(int n){
+        if (n<0 || n>20) {
+            throw new IllegalArgumentException("n must be >= 0 and <= 20");
+            //đưa đầu vào cà chón mình hok thèm trả về 1 giá ám chỉ sai rồi
+            //mình chửi luôn
+        }
+        
+        if (n == 1 || n == 1) {
+            return 1;
+            //sống sót đến dây n > 1 và <= 20 rồi
+        }
+        
+        long result = 1;
+        for (int i = 2; i < n; i++) {
+            result *= i;
+        }
+        
+        return result;
+    }
+}
