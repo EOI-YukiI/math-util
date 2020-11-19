17fc259c749c71f32005e122025d6c8fc06bbd0d
EOI-YukiI <inosiyuki1@gmail.com>
2020-11-19 08:34:15 +0700

    edit MathUtility

--- src/yukii/util/MathUtility.java
+++ src/yukii/util/MathUtility.java
@@ -17,7 +17,27 @@ public class MathUtility {
     //hàm tính n giai thừa. n>= 0 && c<=20
     //nếu quá 20 thì tràn kiểu long
     //0! = 1; 1! =1; n! = 1.2.3...n
-    public static long getFactorial(int n){
+//    public static long getFactorial(int n){
+//        if (n<0 || n>20) {
+//            throw new IllegalArgumentException("n must be >= 0 and <= 20");
+//            //đưa đầu vào cà chón mình hok thèm trả về 1 giá ám chỉ sai rồi
+//            //mình chửi luôn
+//        }
+//        
+//        if (n == 1 || n == 1) {
+//            return 1;
+//            //sống sót đến dây n > 1 và <= 20 rồi
+//        }
+//        
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        
+//        return result;
+//    }
+    
+        public static long getFactorial(int n){
         if (n<0 || n>20) {
             throw new IllegalArgumentException("n must be >= 0 and <= 20");
             //đưa đầu vào cà chón mình hok thèm trả về 1 giá ám chỉ sai rồi
@@ -29,11 +49,6 @@ public class MathUtility {
             //sống sót đến dây n > 1 và <= 20 rồi
         }
         
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        
-        return result;
+        return n * getFactorial(n - 1);
     }
 }
